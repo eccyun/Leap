2c6f0ab8ccb6a07610b90a5cecd6c30dd1f06f60
eccyun <rt.notes0201@gmail.com>
2016-09-24 23:22:09 +0900

    TitleManagerを作って、大元の構造はそっちで管理するようにしました。

---
+++ Leap/Assets/Script/Title/TitleManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.touchCount > 0){
+			Touch   _touch     = Input.GetTouch(0);
+			Vector2 worldPoint = Camera.main.ScreenToWorldPoint(_touch.position);
+
+			// タップ位置にオブジェクトがあるか
+			RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+			if(hit){
+				Bounds rect = hit.collider.bounds;
+				if(rect.Contains(worldPoint)){
+					if(hit.collider.gameObject.name=="START"){
+						Debug.Log("START");
+					}else if(hit.collider.gameObject.name=="QUICK_START"){
+						Debug.Log("QUICKSTART");
+					}else if(hit.collider.gameObject.name=="LOAD"){
+						Debug.Log("LOAD");
+					}
+				}
+			}
+		}
+	}
+}
