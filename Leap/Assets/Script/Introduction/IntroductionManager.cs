c487e7e6e0880aa70b89f4bdad96c804a9108894
eccyun <rt.notes0201@gmail.com>
2016-10-10 00:07:56 +0900

    先日までの進行分をコミット

---
+++ Leap/Assets/Script/Introduction/IntroductionManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IntroductionManager : MonoBehaviour {
+	GameObject drawSprite;
+	int        moveCount;
+	Camera     main;
+
+	// Use this for initialization
+	void Start () {
+		drawSprite = GameObject.FindGameObjectWithTag("MainImage");
+		moveCount  = 1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		main                = GameObject.Find("Main Camera").GetComponent<Camera>();
+		Vector3    touchPos = main.ScreenToWorldPoint(Input.mousePosition);
+		Collider2D col      = Physics2D.OverlapPoint(touchPos);
+
+		if(Input.GetMouseButtonDown(0)){
+			if(col == drawSprite.GetComponent<Collider2D>()){
+				SpriteRenderer renderer  = drawSprite.GetComponent<SpriteRenderer>();
+				MainImage      mainImage = drawSprite.GetComponent<MainImage>();
+
+				if(moveCount==1){
+					renderer.sprite = mainImage.navigation_2;
+				}else if(moveCount==2){
+					renderer.sprite = mainImage.navigation_3;
+				}
+				moveCount++;
+			}
+		}
+	}
+}
---
+++ Leap/Assets/Script/Introduction/MainImage.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainImage : MonoBehaviour {
+	public Sprite navigation_1;
+	public Sprite navigation_2;
+	public Sprite navigation_3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
