9658e46faf0f51b421bbf208ff1fdc56b5b55f72
eccyun <rt.notes0201@gmail.com>
2016-11-13 03:02:44 +0900

    SaveLoadシーンのタッチイベント置き換え

--- Leap/Assets/Script/SaveLoad/SaveLoadManager.cs
+++ Leap/Assets/Script/SaveLoad/SaveLoadManager.cs
@@ -4,26 +4,22 @@ using System.Collections;
 
 public class SaveLoadManager : MonoBehaviour {
 
+	GameObject backBtn;
+
 	// Use this for initialization
 	void Start () {
-
+		backBtn = GameObject.FindGameObjectWithTag("backBtn");
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if(Input.touchCount > 0){
-			Touch   _touch     = Input.GetTouch(0);
-			Vector2 worldPoint = Camera.main.ScreenToWorldPoint(_touch.position);
+		if(Input.GetMouseButtonDown(0)){
+			Camera     main     = GameObject.Find("Main Camera").GetComponent<Camera>();
+			Vector3    touchPos = main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D col      = Physics2D.OverlapPoint(touchPos);
 
-			// タップ位置にオブジェクトがあるか
-			RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
-			if(hit){
-				Bounds rect = hit.collider.bounds;
-				if(rect.Contains(worldPoint)){
-					if(hit.collider.gameObject.name=="save_back"){
-						SceneManager.UnloadScene("SaveLoad");
-					}
-				}
+			if(col == backBtn.GetComponent<Collider2D>()){
+				SceneManager.UnloadScene("SaveLoad");
 			}
 		}
 	}
