9f3f6e26be14d3d1c21c1a1110f974abe3e19f2e
eccyun <rt.notes0201@gmail.com>
2017-02-06 00:12:21 +0900

    edit イベント割り当て処理を追加

--- Leap/Assets/Script/GameMenu/GameMenuManager.cs
+++ Leap/Assets/Script/GameMenu/GameMenuManager.cs
@@ -3,35 +3,59 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class GameMenuManager : MonoBehaviour {
-	private Camera          camera;
-	private MainGameManager mainGameManager;
-	private SceneComponent  sceneComponent;
-	public  string          pushBtnName = "";
+	private Camera               camera;
+	private MainGameManager      mainGameManager;
+	private SceneComponent       sceneComponent;
+	private DialogPanelComponent dialogPanelComponent;
+	public  string               pushBtnName = "";
+	public  GameObject           dialogPanel;
+	public  bool                 action_flg;
 
 	// Use this for initialization
 	void Start () {
-		camera          = GameObject.Find("Main Camera").GetComponent<Camera>();
-		mainGameManager = GameObject.Find("MainGameManager").GetComponent<MainGameManager>();
-		sceneComponent  = GetComponent<SceneComponent> ();
+		camera               = GameObject.Find("Main Camera").GetComponent<Camera>();
+		mainGameManager      = GameObject.Find("MainGameManager").GetComponent<MainGameManager>();
+		sceneComponent       = GameObject.Find("Panel").GetComponent<SceneComponent> ();
+		dialogPanelComponent = dialogPanel.GetComponent<DialogPanelComponent>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0)){
-			Vector3    touchPos = camera.ScreenToWorldPoint(Input.mousePosition);
-			Collider2D col      = Physics2D.OverlapPoint(touchPos);
+		if(dialogPanelComponent.gameObject.activeSelf){
+			return;
+		}
+		if(!action_flg){
+			if(Input.GetMouseButtonDown(0)){
+				Vector3    touchPos = camera.ScreenToWorldPoint(Input.mousePosition);
+				Collider2D col      = Physics2D.OverlapPoint(touchPos);
 
-			if(col==GameObject.Find("back_game").GetComponent<Collider2D>()){
-				mainGameManager.isUpdateStop = false;
-				SceneManager.UnloadScene("GameMenu");
-			}else if(col==GameObject.Find("menu_save").GetComponent<Collider2D>()||col==GameObject.Find("menu_load").GetComponent<Collider2D>()){
-				if(col==GameObject.Find("menu_save").GetComponent<Collider2D>()){
-					pushBtnName = "menu_save";
-				}else if(col==GameObject.Find("menu_load").GetComponent<Collider2D>()){
-					pushBtnName = "menu_load";
+				if(col==GameObject.Find("back_game").GetComponent<Collider2D>()){
+					mainGameManager.isUpdateStop = false;
+					SceneManager.UnloadScene("GameMenu");
+				}else if(col==GameObject.Find("menu_save").GetComponent<Collider2D>()||col==GameObject.Find("menu_load").GetComponent<Collider2D>()){
+					if(col==GameObject.Find("menu_save").GetComponent<Collider2D>()){
+						pushBtnName = "menu_save";
+					}else if(col==GameObject.Find("menu_load").GetComponent<Collider2D>()){
+						pushBtnName = "menu_load";
+					}
+					sceneComponent.pushScene("SaveLoad");
+				}else if(col==GameObject.Find("back_title").GetComponent<Collider2D>()){
+					dialogPanelComponent.show("ゲームを終了しますか？", yesCallBack, noCallBack);
 				}
-				sceneComponent.pushScene("SaveLoad");
+			}
+		}else{
+			if(!sceneComponent.panelComponent.isFade){
+				sceneComponent.moveScene("LOAD");
 			}
 		}
+	}
+
+	public void yesCallBack(){
+		sceneComponent.fade();
+		action_flg = true;
+	}
+
+	public void noCallBack(){
+		dialogPanelComponent.hide();
 	}
 }
