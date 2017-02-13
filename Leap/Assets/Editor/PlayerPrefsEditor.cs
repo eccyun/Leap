db846a393cf661934243d649f8dbb08da0940727
eccyun <rt.notes0201@gmail.com>
2017-02-13 22:48:45 +0900

    add PlayerPrefsをメニューから消せるように調整しました。

---
+++ Leap/Assets/Editor/PlayerPrefsEditor.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerPrefsEditor : MonoBehaviour {
+	[MenuItem("Tools/PlayerPrefs/DeleteAll")]
+    static void DeleteAll(){
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Delete All Data Of PlayerPrefs!!");
+    }
+}
