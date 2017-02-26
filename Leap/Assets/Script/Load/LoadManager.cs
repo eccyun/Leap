b4a774366227320287ff39816965e72d2c8d1826
eccyun <rt.notes0201@gmail.com>
2017-02-26 13:38:20 +0900

    一度タイトルに戻ると、以後ロードができなくなる不具合を修正

--- Leap/Assets/Script/Load/LoadManager.cs
+++ Leap/Assets/Script/Load/LoadManager.cs
@@ -24,6 +24,7 @@ public class LoadManager : MonoBehaviour {
 			}else{
 				sceneComponent.moveScene(scriptEngine.moveLoadedSceneName);
 			}
+			scriptEngine.moveLoadedSceneName = null;
 			return;
 		}
 		frame = frame+range;
